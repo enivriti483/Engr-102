446875828e1873691ab8eaeba1ce89593e4830aa
enivriti483 <enivriti@tamu.edu>
2024-09-04 17:43:33 -0500

    Update make_change.py

--- make_change.py
+++ make_change.py
@@ -1,3 +1,12 @@
-hello
-hello
-
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do"
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Names: Nivriti Eadala
+# Robin El Attar
+# Nithya Vemuri
+# Mirthvika Srinivasan
+# Section: 505
+# Assignment: 4.15 - Make Change
+# Date: 04 September 2024
+# 
