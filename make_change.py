0a9f8e5de4a46f4cedbd73883ad4d2ea7903a265
enivriti483 <enivriti@tamu.edu>
2024-09-04 17:49:59 -0500

    Update make_change.py

--- make_change.py
+++ make_change.py
@@ -10,3 +10,17 @@
 # Assignment: 4.15 - Make Change
 # Date: 04 September 2024
 # 
+amount_paid = float(input(“How much did you pay?: ”))
+cost = float(input(“How much did it cost?: ”))
+change = amount_paid - cost
+print(“Change is:”, change)
+change *= 100
+num_quarters = change // 25
+change -= (num_quarters*25)
+num_dimes = change // 10
+change -= (num_dimes*10)
+num_pennies = change
+
+print(“Num quarters is:”, num_quarters)
+print(“Num dimes is:”, num_dimes)
+print(“Num pennies is:”, num_pennies)
